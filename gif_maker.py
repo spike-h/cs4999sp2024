a0cef257090fbccd399dcd5b0b69c1a1028f9db2
Dyllan Hofflich <53828342+spike-h@users.noreply.github.com>
2024-03-19 18:28:39 -0400

    Update gif_maker.py

--- gif_maker.py
+++ gif_maker.py
@@ -13,11 +13,19 @@ def make_gif(frame_folder):
     
     # Convert the sorted image files to Image objects
     frames = [Image.open(image) for image in sorted_files]
+    # frames = frames[::2]
+
+    frame_one = frames[0]
+    base_width= 350
+    wpercent = (base_width / float(frame_one.size[0]))
+    hsize = int((float(frame_one.size[1]) * float(wpercent)))
+    frames = [img.resize((base_width, hsize)) for img in frames]
+    frames = frames[::2]
     
     # Save the GIF
     frame_one = frames[0]
-    frame_one.save("my_awesome.gif", format="GIF", append_images=frames,
+    frame_one.save("/home/pcglab/Orbit/source/standalone/tutorials/04_sensors/output/camera1.gif", format="GIF", append_images=frames,
                     save_all=True, duration=125/3, loop=0)
 
 if __name__ == "__main__":
-    make_gif("depth/")
+    make_gif("/home/pcglab/Orbit/source/standalone/tutorials/04_sensors/output/camera1")
